2ae68186be78402e8d8ab6d558016c1c5b48dfe6
Mathieu Leduc-Hamel <mathieu.leduchamel@shopify.com>
2015-07-13 17:56:55 -0400

    Bumped version: 0.0.1 -> 0.1.0

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ requires = [
 
 setup(
     name="atelierlaurier",
-    version="0.0.1",
+    version="0.1.0",
     description="L'atelier Laurier",
     long_description=README + "\n\n" + CHANGES,
     classifiers=[
